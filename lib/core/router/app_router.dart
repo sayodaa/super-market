1ba4dc72c817ee2d24ce73ebbdb35f51a03e2694
Mahmoud-Younes <106392771+Mahmoud-Younes@users.noreply.github.com>
2025-05-05 02:21:35 +0300

    add PurchasesScreens

--- lib/core/router/app_router.dart
+++ lib/core/router/app_router.dart
@@ -4,6 +4,7 @@ import 'package:go_router/go_router.dart';
 import 'package:mahmoud_hassan/core/router/route_names.dart';
 import 'package:mahmoud_hassan/features/home/presentation/views/home_screen.dart';
 import 'package:mahmoud_hassan/features/inventory/presentation/views/inentory_screen.dart';
+import 'package:mahmoud_hassan/features/purchases/presentation/views/purchases_screens.dart';
 import 'package:mahmoud_hassan/features/sale/presentation/views/sale_screen.dart';
 
 final router = GoRouter(
@@ -20,6 +21,10 @@ final router = GoRouter(
     GoRoute(
       path: RouteNames.inventory,
       builder: (context, state) => const InventoryScreen(),
+    ),
+    GoRoute(
+      path: RouteNames.purchases,
+      builder: (context, state) => const PurchasesScreens(),
     ),
   ],
 );
